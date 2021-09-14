fdc6aabc20bceb8231be640d452d51c70ee27bf7
oscar-fernandez <68928859+oscar-fernandez@users.noreply.github.com>
2021-09-14 08:16:21 -0400

    Add scripts.js file and fix .navigation-container
    Add scripts.js to move js to separate file. .navigation-container was spelled incorrectly in index.html so the styles were not applying. Removed unnecessary styles from navigation container

---
+++ scripts.js
@@ -0,0 +1,21 @@
+// Get the button
+var theButton = document.getElementById('myButton')
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function () {
+  scrollFunction()
+}
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    theButton.style.display = 'block'
+  } else {
+    theButton.style.display = 'none'
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0
+  document.documentElement.scrollTop = 0
+}
