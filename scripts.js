382701d6fb514cb5b16e02dc7ce7fcbf870540d1
oscar-fernandez <68928859+oscar-fernandez@users.noreply.github.com>
2021-10-13 21:15:13 -0400

    Remove arrow, add overflow scroll, rework scripts

--- scripts.js
+++ scripts.js
@@ -1,29 +1,32 @@
 // Hamburger menu animations
 const click = document.querySelector('.hamburger-menu')
 const navContainer = document.querySelector('.navigation-container')
-const arrow = document.querySelector('.arrow')
+// const arrow = document.querySelector('.arrow')
 const body = document.querySelector('body')
 const header = document.querySelector('header')
 const clickItems = document.getElementsByClassName('table-of-content')
 
-window.addEventListener('resize', () => {
-  if (window.innerWidth < 1025) {
-    click.addEventListener('click', () => {
+const setUpListeners = () => {
+  click.addEventListener('click', () => {
+    click.classList.toggle('open-menu')
+    navContainer.classList.toggle('menu-is-open')
+    body.classList.toggle('noscroll')
+    header.classList.toggle('header-static')
+  })
+  Array.from(clickItems).forEach((element) => {
+    element.addEventListener('click', () => {
       click.classList.toggle('open-menu')
       navContainer.classList.toggle('menu-is-open')
       body.classList.toggle('noscroll')
       header.classList.toggle('header-static')
-      console.log('toggled')
     })
-    Array.from(clickItems).forEach((element) => {
-      element.addEventListener('click', () => {
-        click.classList.toggle('open-menu')
-        navContainer.classList.toggle('menu-is-open')
-        body.classList.toggle('noscroll')
-        header.classList.toggle('header-static')
-      })
-    })
-  } else {
+  })
+}
+
+window.addEventListener('resize', () => {
+  if (window.innerWidth < 1025) {
+    setUpListeners()
+  } else if (window.innerWidth > 1025) {
     click.addEventListener('click', () => {
       return
     })
@@ -34,3 +37,7 @@ window.addEventListener('resize', () => {
     })
   }
 })
+
+window.onload = () => {
+  window.innerWidth < 1025 ? setUpListeners() : null
+}
